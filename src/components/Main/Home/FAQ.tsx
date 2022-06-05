ef72a005b3ea2fb39b97f6708c09e760c14289c0
Stepanishin <stepanishin92@mail.ru>
2022-06-05 08:41:50 +0200

    v_0.17

--- src/components/Main/Home/FAQ.tsx
+++ src/components/Main/Home/FAQ.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// import 'antd/dist/antd.css';
 import 'antd/lib/collapse/style/index.css'
 import './FAQ.css'
 import { Collapse } from 'antd';
@@ -11,26 +10,7 @@ const FAQ: React.FC<{}>= () => {
 
     const { Panel } = Collapse;
 
-    // const show_descr = (e) => {
-    //     const descr = document.querySelector('.FAQ_item_description');
-    //     descr?.classList.toggle('dn')
-    // }
-
-    // const onChange = (key: any) => {
-    // console.log(key);
-    // };
-
     return (
-        // <div className='FAQ'>
-            /* <ul>
-                <li>
-                    <div className='FAQ_item' onClick={show_descr}>
-                        <p className='FAQ_item_title' >What is “I know”?</p>
-                        <p className='FAQ_item_description' >“I Know” is a first web3 betting platform in NFTs world. You have to hold the “Iknow betTER”  NFT and Iknow DAOTiket(NFT2) on the wallet to get more benefits and earn digital money.</p>
-                    </div>
-                </li>
-            </ul> */
-        /* </div> */
         <Collapse
             bordered={false}
             expandIcon={({ isActive }) => <CaretRightOutlined rotate={isActive ? 90 : 0} />}
@@ -49,8 +29,32 @@ const FAQ: React.FC<{}>= () => {
             In the future, NFTs from Iknow betTER collection will provide an opportunity to get exclusive  "Iknow DAOTicket(NFT2)”.
             </p>
             </Panel>
-            <Panel header="This is panel header 3" key="3" className="FAQ_item">
-            <p></p>
+
+            <Panel header="What is a Iknow DAOTiket(NFT2)?" key="3" className="FAQ_item">
+            <p>This is an exclusive NFT collection, holders of an "Iknow DAOTicket(NFT2)” get access to earn digital money and our discord-based DAO, IKnowDAO, where we deliver interconnectivity via social programs and rewards to the community.</p>
+            </Panel>
+
+            <Panel header="How to get a spot in the Iknow betTER whitelist?" key="4" className="FAQ_item">
+            <p>All active, helpful members of the community who show a passion for the project will get a spot in the Iknow betTER collection whitelist. Activity is encouraged, but we’re not going to look at the number of discord invites or your level on the server. It is important for us to create an engaged community passionate about our product.</p>
+            </Panel>
+
+            <Panel header="When, where, and how will the mint take place?" key="5" className="FAQ_item">
+            <p>We plan to launch the Iknow betTER collection in the second half of september 2022 on one of the largest launchpads. Details about this launch will be announced later.</p>
+            </Panel>
+
+            <Panel header="When and where will the collection be listed?" key="6" className="FAQ_item">
+            <p>The collection will be listed in the next few hours or even minutes after the mint. We do not plan to list the collection on a large number of marketplaces. We choose quality instead of quantity. We recommend you follow the announcements on our Twitter and Discord.</p>
+            </Panel>
+
+            <Panel header="Is there a limit per wallet?" key="7" className="FAQ_item">
+            <p>Early adopters (OG) limit — 2 mints per 1 wallet. 
+            Whitelist (WL) limit — 1 mint per 1 wallet. 
+            There is no planned limit for the public WL.
+            </p>
+            </Panel>
+
+            <Panel header="How do I find out the rarity of my NFT? Does it affect anything?" key="8" className="FAQ_item">
+            <p>The rarity of the NFTs will be available as soon as possible after the mint on our website. There will also be an NFT rarity check by its number on the website. You will just have to enter the number of the NFT, and you will see its traits and Global Ranking. We will also provide official information on rarity to all popular services as soon as possible after the mint.</p>
             </Panel>
       </Collapse>
     );
---
+++ src/components/Main/Home/Roadmap/Roadmap.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+// import 'antd/dist/antd.css';
+import './Roadmap.css';
+// import 'antd/lib/timeline/style/index.css'
+import { Timeline } from 'antd';
+import { ClockCircleOutlined } from '@ant-design/icons';
+
+const Roadmap: React.FC = () => {
+    return (
+        <Timeline mode="alternate">
+        <Timeline.Item>
+            Project start - May 2022. <br />
+            The final vision of the project concept and search for artists.
+        </Timeline.Item>
+
+        <Timeline.Item color="green">
+          Partnerships & Advisors - June 2022. <br />
+          Deals with potential partners from traditional and crypto markets
+          Team creation
+        </Timeline.Item>
+
+        <Timeline.Item>
+          Social networks launch - July 2022. <br />
+          Site development
+          Community building by launching social activity in Discord, Twitter and Telegram
+        </Timeline.Item>
+
+        <Timeline.Item>
+            NFT development - July 2022. <br />
+            We're building tools that allow consumers to bet NFTs collections, for creators and developers to promote new NFTs collections in the digital marketplaces world.
+            Selection of the best artists, active development of the arts, and preparation for the Iknow betTER collection mint.
+        </Timeline.Item>
+
+        <Timeline.Item>
+            Iknow betTER collection mint - September 2022. <br />
+            888 Iknow betTER NFT for the price of 0.88 SOL are distributed to their new owners. The Iknow betTER is a 100% WhiteList project. We will provide a pre-mint of 88 NFTs for marketing purposes, as well as give out whitelist spots to the most active members of the community and members of the largest DAOs in the Solana ecosystem.
+        </Timeline.Item>
+
+        <Timeline.Item>
+            Launch of the Iknow platform - September 2022. <br />
+            “I Know” is a community-centric platform. We work hard to be responsive, close to the ground, and in service to the best interests of our users. 
+        </Timeline.Item>
+
+        <Timeline.Item>
+            NFT Snapshot - september - december 2022. <br />
+            We will periodically take random snapshots of our Iknow betTER NFT holders' wallets. In the future, all wallets that hold at least two  Iknow betTER NFTs during each of the snapshots will receive an opportunity to get Iknow DAOTiket(NFT2).
+        </Timeline.Item>
+
+        <Timeline.Item>
+            Development of the Iknow DAOTiket(NFT2) collection  - september 2022 <br />
+        </Timeline.Item>
+
+        <Timeline.Item>
+            Iknow DAOTiket chat(NFT2) collection mint - october 2022. <br />
+            88 Iknow DAOTicket(NFT2) for the price of 8 SOL are distributed to their new owners.
+            Holding a "Iknow DAOTicket(NFT2)” gives holders access to our discord-based DAO, IKnowDAO, where we deliver interconnectivity via social programs and rewards to the community.
+        </Timeline.Item>
+
+        <Timeline.Item>
+            Launch of the DAO and Iknow DAOTiket(NFT2) chat for the holders - december 2022 <br />
+        </Timeline.Item>
+        
+      </Timeline>
+    );
+};
+
+export default Roadmap;
