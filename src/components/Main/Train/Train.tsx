044832942941da5be12d06866712c3482ae6767c
Stepanishin <stepanishin92@mail.ru>
2022-06-18 21:43:19 +0200

    v_0.17

--- src/components/Main/Train/Train.tsx
+++ src/components/Main/Train/Train.tsx
@@ -35,50 +35,13 @@ const Train = () => {
 
     return (
         <>
-        {/* <Context> */}
             <Content />
-        {/* </Context> */}
-        {/* <Train2 /> */}
         </>
     );
 };
 
 export default Train;
 
-// const Context: FC<{ children: ReactNode }> = ({ children }) => {
-//     // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
-//     const network = WalletAdapterNetwork.Mainnet;
-
-//     // You can also provide a custom RPC endpoint.
-//     const endpoint = useMemo(() => clusterApiUrl(network), [network]);
-
-//     // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
-//     // Only the wallets you configure here will be compiled into your application, and only the dependencies
-//     // of wallets that your users connect to will be loaded.
-//     const wallets = useMemo(
-//         () => [
-//             new LedgerWalletAdapter(),
-//             new PhantomWalletAdapter(),
-//             new GlowWalletAdapter(),
-//             new SlopeWalletAdapter(),
-//             new SolletExtensionWalletAdapter(), 
-//             new SolletWalletAdapter(),
-//             new SolflareWalletAdapter({ network }),
-//             new TorusWalletAdapter(),
-//         ],
-//         [network]
-//     );
-
-   
-
-//     return (
-//         <ConnectionProvider endpoint={endpoint}>
-//             <WalletProvider wallets={wallets} autoConnect>
-//                 <WalletModalProvider>{children}</WalletModalProvider>
-//             </WalletProvider>
-//         </ConnectionProvider>
-//     );
-// };
 
 const Content: FC = () => {
     let [lamports, setLamports] = useState(.1);
@@ -119,22 +82,21 @@ const Content: FC = () => {
     }, [publicKey, sendTransaction, connection]);
 
     
-function setTheLamports(e: any)
-{
-    console.log(Number(e.target.value));
-    setLamports(Number(e.target.value));
-    lamports = e.target.value;
-    thelamports = lamports;
-}
-function setTheWallet(e: any){
-    setWallet(e.target.value)
-    theWallet = e.target.value;
-}
+    function setTheLamports(e: any)
+    {
+        console.log(Number(e.target.value));
+        setLamports(Number(e.target.value));
+        lamports = e.target.value;
+        thelamports = lamports;
+    }
+    function setTheWallet(e: any){
+        setWallet(e.target.value)
+        theWallet = e.target.value;
+    }
 
     return (
         <div className="App">
     
-            {/* <WalletMultiButton /> */}
             <input value={lamports} type="number" onChange={(e) => setTheLamports(e)}></input>
             <br></br>
             <button className='btn' onClick={onClick}>Send Sol </button>
