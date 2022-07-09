8095b6ad70eb705a9708770ed9ca8aa358407c55
Stepanishin <stepanishin92@mail.ru>
2022-07-09 14:57:35 +0200

    v_0.28

--- src/components/Main/List/List.tsx
+++ src/components/Main/List/List.tsx
@@ -46,48 +46,53 @@ const List: FC = () => {
         }).catch((error) => {
         console.error(error);
         });
-    }
-    
-    
+    }   
 
     return (
         <div className='list'>
-
-            <h2 className='List_container_title List_container_title_active'>Active Judge</h2>
-            <div className='List_cards'>
+            
                 {
                     cards.map(card => {   
                         if (card[1].state === 'active') {
                         return (
-                            <div className='list_card' key={card[1].name}>
-                                <Link to={`/List/${card[1].name}`} >
-                                    <img className='list_avatar' src={card[1].avatar} alt="" />
-                                    <h3 className='list_title'>{card[1].name}</h3>
-                                </Link>
-                            </div>
+                            <>
+                                <h2 className='List_container_title List_container_title_active'>Active Judge</h2>
+                                <div className='List_cards'>
+                                    <div className='list_card' key={card[1].name}>
+                                        <Link to={`/List/${card[1].name}`} >
+                                            <img className='list_avatar' src={card[1].avatar} alt="" />
+                                            <h3 className='list_title'>{card[1].name}</h3>
+                                        </Link>
+                                    </div>
+                                </div>
+                            </>
                         )
                         }
                     })
                 }
-            </div>
 
-            <h2 className='List_container_title List_container_title_wait'>Waiting for results </h2>
-            <div className='List_cards'>
+
+            
                 {
                     cards.map(card => {
                         if (card[1].state === 'wait') {
                         return (
-                            <div className='list_card' key={card[1].name}>
-                                <Link to={`/List/${card[1].name}`} >
-                                    <img className='list_avatar' src={card[1].avatar} alt="" />
-                                    <h3 className='list_title'>{card[1].name}</h3>
-                                </Link>
-                            </div>
+                            <>
+                                <h2 className='List_container_title List_container_title_wait'>Waiting for results </h2>
+                                <div className='List_cards'>
+                                <div className='list_card' key={card[1].name}>
+                                    <Link to={`/List/${card[1].name}`} >
+                                        <img className='list_avatar' src={card[1].avatar} alt="" />
+                                        <h3 className='list_title'>{card[1].name}</h3>
+                                    </Link>
+                                </div>
+                                </div>
+                            </>
                         )
                         }
                     })
                 }
-            </div>
+            
 
             <h2 className='List_container_title'>Past Judge</h2>
             <div className='List_cards'>
