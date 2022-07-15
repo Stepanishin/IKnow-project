8126e497851371b2e030868e380df901071c2365
Stepanishin <stepanishin92@mail.ru>
2022-07-15 11:32:45 +0200

    v_0.33

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Logo from '../UI/Logo';
+import Logo from '../UI/Logo/Logo';
 import './Footer.css'
 
 const Footer = () => {
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import React, { FC, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import Logo from '../UI/Logo';
+import Logo from '../UI/Logo/Logo';
 import ConnectWallet from '../UI/ConnectWallet/ConnectWallet';
 import './Header.css'
 
